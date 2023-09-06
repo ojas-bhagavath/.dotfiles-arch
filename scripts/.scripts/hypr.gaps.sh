7592ec65a03456174d345e73de350bf67763c13d
Ojas Bhagavath <ojas.bhagavath@gmail.com>
2023-09-06 09:57:06 +0530

    Added scripts folder to manage scripts

--- scripts/.scripts/hypr.gaps.sh
+++ scripts/.scripts/hypr.gaps.sh
@@ -2,8 +2,8 @@
 HYPRGAMEMODE=$(hyprctl getoption general:gaps_in | awk 'NR==2{print $2}')
 if [ "$HYPRGAMEMODE" = 0 ] ; then
     hyprctl --batch "\
-        keyword general:gaps_in 5;\
-        keyword general:gaps_out 10;\
+        keyword general:gaps_in 7;\
+        keyword general:gaps_out 14;\
         keyword decoration:rounding 5"
     exit
 fi
