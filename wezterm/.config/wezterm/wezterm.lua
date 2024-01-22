1a55ddb40ba5b08a652e1d3a7cca53f1596a9df8
Ojas Bhagavath <ojas.bhagavath@gmail.com>
2024-01-22 21:58:14 +0530

    Added wezterm config

---
+++ wezterm/.config/wezterm/wezterm.lua
@@ -0,0 +1,24 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This table will hold the configuration.
+local config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+	config = wezterm.config_builder()
+end
+
+-- This is where you actually apply your config choices
+
+-- For example, changing the color scheme:
+config.color_scheme = "tokyonight_moon"
+config.font = wezterm.font("Hack Nerd Font")
+config.font_size = 16
+config.enable_scroll_bar = true
+config.hide_tab_bar_if_only_one_tab = true
+config.window_close_confirmation = "NeverPrompt"
+
+-- and finally, return the configuration to wezterm
+return config
