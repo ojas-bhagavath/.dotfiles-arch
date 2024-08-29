3076cd2c2af71831263c693419a74f72c7dae677
Ojas Bhagavath <ojas.bhagavath@gmail.com>
2024-08-29 11:09:04 +0530

    Housekeeping

--- nvim/.config/nvim/lua/core/plugins/completion.lua
+++ nvim/.config/nvim/lua/core/plugins/completion.lua
@@ -2,9 +2,9 @@ return { -- Autocompletion
     "hrsh7th/nvim-cmp",
     event = "InsertEnter",
     dependencies = {
+        "saadparwaiz1/cmp_luasnip",
         "hrsh7th/cmp-buffer",
         "hrsh7th/cmp-path",
-        "saadparwaiz1/cmp_luasnip",
         "hrsh7th/cmp-nvim-lsp",
     },
     config = function()
@@ -21,7 +21,6 @@ return { -- Autocompletion
                 completion = cmp.config.window.bordered(),
                 documentation = cmp.config.window.bordered(),
             },
-
             -- For an understanding of why these mappings were
             -- chosen, you will need to read `:help ins-completion`
             --
@@ -50,33 +49,6 @@ return { -- Autocompletion
                 -- Manually trigger a completion from nvim-cmp. Generally you don't need this, because nvim-cmp will display
                 --  completions whenever it has completion options available.
                 -- ["<C-y>"] = cmp.mapping.complete({}),
-
-                -- Think of <c-l> as moving to the right of your snippet expansion.
-                --  So if you have a snippet that's like:
-                --  function $name($args)
-                --    $body
-                --  end
-                --
-                -- <c-l> will move you to the right of each of the expansion locations.
-                -- <c-h> is similar, except moving you backwards.
-                -- ["<C-l>"] = cmp.mapping(function()
-                --     if luasnip.expand_or_locally_jumpable() then
-                --         luasnip.expand_or_jump()
-                --     end
-                -- end, { "i", "s" }),
-                -- ["<C-h>"] = cmp.mapping(function()
-                --     if luasnip.locally_jumpable(-1) then
-                --         luasnip.jump(-1)
-                --     end
-                -- end, { "i", "s" }),
-                -- ["<C-e>"] = cmp.mapping(function()
-                --     if luasnip.choice_active() then
-                --         luasnip.change_choice(1)
-                --     end
-                -- end, { "i", "s" }),
-                --
-                -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
-                --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
             }),
             sources = cmp.config.sources({
                 { name = "luasnip" }, -- snippets
