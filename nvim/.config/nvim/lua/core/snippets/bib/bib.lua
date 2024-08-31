e55181d9062331d0910bcdbe8972484a5a3217f2
Ojas Bhagavath <ojas.bhagavath@gmail.com>
2024-08-31 17:04:39 +0530

    Added bib file snippet

---
+++ nvim/.config/nvim/lua/core/snippets/bib/bib.lua
@@ -0,0 +1,27 @@
+return {
+    s(
+        { trig = "book", snippetType = "autosnippet" },
+        fmta(
+            [[
+@book{<>,
+    title = {<>},
+    edition = {<>},
+    author = {<>},
+    year = {<>},
+    publisher = {<>},
+}
+
+<>
+            ]],
+            {
+                i(1),
+                i(2),
+                i(3),
+                i(4),
+                i(5),
+                i(6),
+                i(7),
+            }
+        )
+    ),
+}
