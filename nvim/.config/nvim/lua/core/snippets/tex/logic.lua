64303f355e462af936f39b123100b6a12abb7cfd
Ojas Bhagavath <ojas.bhagavath@gmail.com>
2024-09-13 22:34:07 +0530

    Modified tex snippets

--- nvim/.config/nvim/lua/core/snippets/tex/logic.lua
+++ nvim/.config/nvim/lua/core/snippets/tex/logic.lua
@@ -7,10 +7,7 @@ return {
     s({ trig = "orr", snippetType = "autosnippet" }, t("\\lor"), { condition = in_mathzone }),
     s({ trig = "andd", snippetType = "autosnippet" }, t("\\land"), { condition = in_mathzone }),
     s({ trig = "forall", snippetType = "autosnippet" }, t("\\forall"), { condition = in_mathzone }),
-    s({ trig = "exists", snippetType = "autosnippet" }, t("\\exists"), { condition = in_mathzone }),
-    s({ trig = "since", snippetType = "autosnippet" }, t("\\because"), { condition = in_mathzone }),
+    s({ trig = "is", snippetType = "autosnippet" }, t("\\exists"), { condition = in_mathzone }),
     s({ trig = ":.", snippetType = "autosnippet" }, t("\\because"), { condition = in_mathzone }),
-    s({ trig = "because", snippetType = "autosnippet" }, t("\\because"), { condition = in_mathzone }),
-    s({ trig = "therefore", snippetType = "autosnippet" }, t("\\therefore"), { condition = in_mathzone }),
     s({ trig = ".:", snippetType = "autosnippet" }, t("\\therefore"), { condition = in_mathzone }),
 }
