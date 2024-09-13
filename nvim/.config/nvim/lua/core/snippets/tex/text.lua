6516b81b1991bbd023280a9f2cfdac44bc0d3557
Ojas Bhagavath <ojas.bhagavath@gmail.com>
2024-09-13 22:33:50 +0530

    Added text mode snippets

---
+++ nvim/.config/nvim/lua/core/snippets/tex/text.lua
@@ -0,0 +1,7 @@
+local in_textzone = function()
+    return vim.fn["vimtex#syntax#in_mathzone"]() == 0
+end
+return {
+    s({ trig = "wrt", snippetType = "autosnippet" }, t("with respect to"), { condition = in_textzone }),
+    s({ trig = "tfae", snippetType = "autosnippet" }, t("the following are equivalent:"), { condition = in_textzone }),
+}
