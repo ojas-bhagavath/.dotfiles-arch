294b58e4e25dc4242a694394cba42537c45f9b31
Ojas Bhagavath <ojas.bhagavath@gmail.com>
2024-11-07 15:26:25 +0530

    Update wezterm config

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -2,20 +2,28 @@ local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 
-config.adjust_window_size_when_changing_font_size = false
-config.automatically_reload_config = true
-config.bold_brightens_ansi_colors = "BrightAndBold"
-config.check_for_updates = false
-config.color_scheme = "Tokyo Night Moon"
-config.enable_scroll_bar = true
-config.enable_wayland = false
-config.font = wezterm.font("Hack Nerd Font")
-config.font_size = 16
-config.hide_tab_bar_if_only_one_tab = true
-config.initial_cols = 120
-config.initial_rows = 35
-config.warn_about_missing_glyphs = false
-config.window_close_confirmation = "NeverPrompt"
-config.window_decorations = "RESIZE"
+config = {
+	adjust_window_size_when_changing_font_size = false,
+	automatically_reload_config = true,
+	bold_brightens_ansi_colors = "BrightAndBold",
+	check_for_updates = false,
+	color_scheme = "Tokyo Night Moon",
+	cursor_blink_ease_out = "Constant",
+	cursor_blink_ease_in = "Constant",
+	cursor_blink_rate = 600,
+	enable_kitty_graphics = true,
+	enable_scroll_bar = true,
+	enable_wayland = false,
+	font = wezterm.font("Hack Nerd Font"),
+	font_size = 16,
+	hide_tab_bar_if_only_one_tab = true,
+	initial_cols = 120,
+	initial_rows = 35,
+	max_fps = 120,
+	term = "xterm-kitty",
+	warn_about_missing_glyphs = false,
+	window_close_confirmation = "NeverPrompt",
+	window_decorations = "RESIZE",
+}
 
 return config
