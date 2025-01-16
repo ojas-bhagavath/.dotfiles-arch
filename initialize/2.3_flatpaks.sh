2745eac7751a247a3225ba5ab5c1b9d07803f75c
Ojas Bhagavath <ojas.bhagavath@gmail.com>
2025-01-16 12:26:29 +0530

    Update initialize package lists

--- initialize/2.3_flatpaks.sh
+++ initialize/2.3_flatpaks.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-flatpak install flathub com.usebottles.bottles     # A utility to run windows software on linux in a containerized way
 flatpak install flathub com.github.tchx84.Flatseal # A gui to manage permissions for flatpak applications
-flatpak install flathub org.keepassxc.KeePassXC    # A gui frontend for keepassxc password manager
+flatpak install flathub com.usebottles.bottles     # A utility to run windows software on linux in a containerized way
+flatpak install flathub io.otsaloma.gaupol         # A tool to manipulate srt files
 flatpak install flathub org.gimp.GIMP              # GNU image manipulation program
+flatpak install flathub org.keepassxc.KeePassXC    # A gui frontend for keepassxc password manager
+flatpak install flathub org.videolan.VLC           # VLC Media Player
