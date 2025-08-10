c4e96dc1479cade9f63a5705f7d367345f21fd0d
Ojas Bhagavath <ojas.bhagavath@gmail.com>
2025-08-10 10:20:24 -0500

    fix(config.py): update amazon search engine URL to use us domain instead of in chore(quickmarks): remove unused quickmarks entries

--- qutebrowser/.config/qutebrowser/config.py
+++ qutebrowser/.config/qutebrowser/config.py
@@ -482,7 +482,7 @@ c.url.searchengines = {
     "1337x": "https://1337x.to/search/{}/1/",
     "aa": "https://annas-archive.org/search?q={}",
     "abb": "https://audiobookbay.is/?s={}&tt=1",
-    "am": "https://www.amazon.in/s?k={}",
+    "am": "https://www.amazon.com/s?k={}",
     "ar": "https://archive.org/search?query={}",
     "at": "https://academictorrents.com/browse.php?search={}",
     "aur": "https://aur.archlinux.org/packages?O=0&K={}",
