cc84fee6280143828262d1a7d09c83c5edee61d9
Ojas Bhagavath <ojas.bhagavath@gmail.com>
2026-02-16 11:23:17 -0600

    20260216-11:23

--- qutebrowser/.config/qutebrowser/config.py
+++ qutebrowser/.config/qutebrowser/config.py
@@ -536,7 +536,7 @@ c.url.searchengines = {
     "ytm": "https://music.youtube.com/search?q={}",
     "yts": "https://yts.lt/browse-movies/{}/all/all/0/latest/0/all",
 }
-c.url.start_pages = "https://monkeytype.com/"
+c.url.start_pages = "https://calendar.google.com/calendar/u/0/r/day"
 c.url.yank_ignored_parameters = [
     "ref",
     "utm_source",
