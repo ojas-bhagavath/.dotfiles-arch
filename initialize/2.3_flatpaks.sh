333e6b5fd47867d1c86d40b2d3f4edb86f459be0
Ojas Bhagavath <ojas.bhagavath@gmail.com>
2026-07-31 15:41:28 -0500

    20260731-15:41

--- initialize/2.3_flatpaks.sh
+++ initialize/2.3_flatpaks.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+flatpak install flathub com.bambulab.BambuStudio           # Slicer software for Bambu Lab 3D printers
+flatpak install flathub com.prusa3d.PrusaSlicer            # Slicer software for Prusa (and other) 3D printers
+flatpak install flathub org.bunkus.mkvtoolnix-gui          # GUI to create, alter, and inspect Matroska (mkv) files
 flatpak install flathub org.gimp.GIMP                      # GNU image manipulation program
 flatpak install flathub org.gimp.GIMP.Plugin.Resynthesizer # Resynthesizer plugin for gimp
+flatpak install flathub org.openscad.OpenSCAD              # Script-based parametric 3D CAD modeler
+flatpak install flathub org.qbittorrent.qBittorrent        # Open source BitTorrent client
 flatpak install flathub us.zoom.Zoom                       # Zoom videoconferencing application
