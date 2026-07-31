8954bc85c35a45d6ddf7222764e6b5e793a3c90a
Ojas Bhagavath <ojas.bhagavath@gmail.com>
2026-07-31 17:44:11 -0500

    20260731-17:44

--- initialize/4_misc.sh
+++ initialize/4_misc.sh
@@ -2,3 +2,6 @@
 
 sudo sed -i 's/GRUB_TIMEOUT=.*/GRUB_TIMEOUT=0/g' /etc/default/grub
 sudo grub-mkconfig -o /boot/grub/grub.cfg
+
+sudo cp -f src/config.toml /etc/greetd/config.toml
+sudo cp -f src/greeter.toml /var/lib/noctalia-greeter/greeter.toml
