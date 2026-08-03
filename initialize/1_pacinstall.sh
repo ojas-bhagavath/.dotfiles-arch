5d19ef37de2c50f5bd3eb1480041c85ea6d8013a
Ojas Bhagavath <ojas.bhagavath@gmail.com>
2026-08-03 17:30:26 -0500

    20260803-17:30

--- initialize/1_pacinstall.sh
+++ initialize/1_pacinstall.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo pacman -S $(comm -12 <(pacman -Slq | sort) <(sed "s/\s.*//g" pkglist.txt | sort)) --needed
+sudo pacman -S '$(comm -12 <(pacman -Slq | sort) <(sed "s/\s.*//g" pkglist.txt | sort))' --needed
